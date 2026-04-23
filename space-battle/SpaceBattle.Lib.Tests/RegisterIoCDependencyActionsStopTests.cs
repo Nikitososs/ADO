b1648fdec0b3a4b1754b78e3ffaa6361257cb807
Nikitososs <nsmirnow.n@gmail.com>
2026-04-23 20:16:31 +0600

    test: correct dependency resolving

--- space-battle/SpaceBattle.Lib.Tests/RegisterIoCDependencyActionsStopTests.cs
+++ space-battle/SpaceBattle.Lib.Tests/RegisterIoCDependencyActionsStopTests.cs
@@ -1,6 +1,7 @@
 
 using App;
 using App.Scopes;
+using Moq;
 using System.Collections.Generic;
 using Xunit;
 
@@ -16,6 +17,9 @@ public class RegisterIoCDependencyActionsStopTests
     [Fact]
     public void Execute_ShouldRegisterActionsStop_AndResolveSuccessfully()
     {
+        var activeOperations = new Dictionary<object, SpaceBattle.lib.ICommand>();
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Actions.ActiveOperations", (object[] args) => activeOperations).Execute();
+
         var registration = new SpaceBattle.lib.RegisterIoCDependencyActionsStop();
 
         registration.Execute();
@@ -29,5 +33,29 @@ public class RegisterIoCDependencyActionsStopTests
 
         Assert.NotNull(command);
         Assert.IsType<SpaceBattle.lib.ActionStopCommand>(command);
+    }
+
+    [Fact]
+    public void ActionStopCommand_Execute_ShouldRemoveOperationFromActiveOperations()
+    {
+        var activeOperations = new Dictionary<object, SpaceBattle.lib.ICommand>();
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Actions.ActiveOperations", (object[] args) => activeOperations).Execute();
+
+        var registration = new SpaceBattle.lib.RegisterIoCDependencyActionsStop();
+        registration.Execute();
+
+        var target = new object();
+        var operation = new Mock<SpaceBattle.lib.ICommand>();
+        activeOperations[target] = operation.Object;
+
+        IDictionary<string, object> order = new Dictionary<string, object>
+        {
+            { "target", target }
+        };
+
+        var command = Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.Stop", order);
+        command.Execute();
+
+        Assert.False(activeOperations.ContainsKey(target));
     }
 }
