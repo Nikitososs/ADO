8e2c5853bf9f254327d87f886fb01aca641e7e63
Nikitososs <nsmirnow.n@gmail.com>
2026-04-23 19:50:54 +0600

    fix: registration

--- space-battle/SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
+++ space-battle/SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
@@ -8,9 +8,12 @@ public class RegisterIoCDependencyActionsStart : SpaceBattle.lib.ICommand
 {
     public void Execute()
     {
+        new RegisterIoCDependencyMacroCommand().Execute();
+        new RegisterIoCDependencySendCommand().Execute();
+
         Ioc.Resolve<App.ICommand>("IoC.Register", "Actions.StartCommand", (object[] args) =>
         {
-            return new ActionStartCommand(args);
+            return new ActionStartCommand((SpaceBattle.lib.ICommand)args[0], (ICommandReceiver)args[1]);
         }).Execute();
 
         Ioc.Resolve<App.ICommand>("IoC.Register", "Actions.Start", (object[] args) =>
@@ -18,8 +21,10 @@ public class RegisterIoCDependencyActionsStart : SpaceBattle.lib.ICommand
             var order = (IDictionary<string, object>)args[0];
             var target = order["target"];
             var command = (string)order["command"];
+            var receiver = (ICommandReceiver)order["receiver"];
+            var longRunningOperation = Ioc.Resolve<SpaceBattle.lib.ICommand>(command, target);
 
-            return Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.StartCommand", target, command);
+            return Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.StartCommand", longRunningOperation, receiver);
         }).Execute();
     }
 }
