71f4f582bd0006bf70229ef9b0069c7f9fa00b5d
Nikitososs <nsmirnow.n@gmail.com>
2026-04-23 19:54:38 +0600

    test: test dependency resolving and executing

--- space-battle/SpaceBattle.Lib.Tests/RegisterIoCDependencyActionsStartTests.cs
+++ space-battle/SpaceBattle.Lib.Tests/RegisterIoCDependencyActionsStartTests.cs
@@ -17,19 +17,50 @@ public class RegisterIoCDependencyActionsStartTests
     [Fact]
     public void Execute_ShouldRegisterActionsStart_AndResolveSuccessfully()
     {
+        var operation = new Mock<SpaceBattle.lib.ICommand>();
+        var receiver = new Mock<SpaceBattle.lib.ICommandReceiver>();
+        var targetObject = new object();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Move", (object[] args) => operation.Object).Execute();
+
         var registration = new SpaceBattle.lib.RegisterIoCDependencyActionsStart();
 
         registration.Execute();
 
         IDictionary<string, object> order = new Dictionary<string, object>
         {
-            { "target", new object() },
-            { "command", "Move" }
+            { "target", targetObject },
+            { "command", "Move" },
+            { "receiver", receiver.Object },
         };
 
         var command = Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.Start", order);
 
         Assert.NotNull(command);
         Assert.IsType<SpaceBattle.lib.ActionStartCommand>(command);
+    }
+
+    [Fact]
+    public void ActionStartCommand_ExecutesOperation_AndEnqueuesItself()
+    {
+        var operation = new Mock<SpaceBattle.lib.ICommand>();
+        var receiver = new Mock<SpaceBattle.lib.ICommandReceiver>();
+        var targetObject = new object();
+
+        Ioc.Resolve<App.ICommand>("IoC.Register", "Move", (object[] args) => operation.Object).Execute();
+        new SpaceBattle.lib.RegisterIoCDependencyActionsStart().Execute();
+
+        IDictionary<string, object> order = new Dictionary<string, object>
+        {
+            { "target", targetObject },
+            { "command", "Move" },
+            { "receiver", receiver.Object },
+        };
+
+        var startCommand = Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.Start", order);
+        startCommand.Execute();
+
+        operation.Verify(op => op.Execute(), Times.Once);
+        receiver.Verify(r => r.Receive(It.Is<SpaceBattle.lib.ICommand>(c => ReferenceEquals(c, startCommand))), Times.Once);
     }
 }
