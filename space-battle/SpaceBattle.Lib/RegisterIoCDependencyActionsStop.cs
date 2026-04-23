fde9ad9d10abc1cd14c65430fc816760298cb783
Nikitososs <nsmirnow.n@gmail.com>
2026-04-23 20:15:51 +0600

    fix: registration stop command in ioc

--- space-battle/SpaceBattle.Lib/RegisterIoCDependencyActionsStop.cs
+++ space-battle/SpaceBattle.Lib/RegisterIoCDependencyActionsStop.cs
@@ -10,15 +10,21 @@ public class RegisterIoCDependencyActionsStop : SpaceBattle.lib.ICommand
     {
         Ioc.Resolve<App.ICommand>("IoC.Register", "Actions.StopCommand", (object[] args) =>
         {
-            return new ActionStopCommand(args);
+            return new ActionStopCommand(
+                (IDictionary<object, SpaceBattle.lib.ICommand>)args[0],
+                args[1]
+            );
         }).Execute();
 
         Ioc.Resolve<App.ICommand>("IoC.Register", "Actions.Stop", (object[] args) =>
         {
             var order = (IDictionary<string, object>)args[0];
             var target = order["target"];
+            var activeOperations = order.TryGetValue("activeOperations", out var activeOperationsObj)
+                ? (IDictionary<object, SpaceBattle.lib.ICommand>)activeOperationsObj
+                : Ioc.Resolve<IDictionary<object, SpaceBattle.lib.ICommand>>("Actions.ActiveOperations");
 
-            return Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.StopCommand", target);
+            return Ioc.Resolve<SpaceBattle.lib.ICommand>("Actions.StopCommand", activeOperations, target);
         }).Execute();
     }
 }
