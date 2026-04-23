e3f4525f5d686a54991fb2568332d1c269446985
Nikitososs <nsmirnow.n@gmail.com>
2026-04-23 20:14:52 +0600

    feat: impl actions stop

--- space-battle/SpaceBattle.Lib/ActionStopCommand.cs
+++ space-battle/SpaceBattle.Lib/ActionStopCommand.cs
@@ -1,17 +1,21 @@
 
+using System.Collections.Generic;
+
 namespace SpaceBattle.lib;
 
 public class ActionStopCommand : SpaceBattle.lib.ICommand
 {
+    private readonly IDictionary<object, ICommand> _activeOperations;
     private readonly object _target;
 
-    public ActionStopCommand(params object[] args)
+    public ActionStopCommand(IDictionary<object, ICommand> activeOperations, object target)
     {
-        _target = args[0];
+        _activeOperations = activeOperations;
+        _target = target;
     }
 
     public void Execute()
     {
-
+        _activeOperations.Remove(_target);
     }
 }
